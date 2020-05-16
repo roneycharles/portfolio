c6dfbde19512964a25adbf7008164b1d0f5531ea
roneycharles <mmn.charles@@gmail.com>
2020-05-16 14:35:53 -0300

    Updates

---
+++ precache-manifest.0829fb0f7f1848dda77e1611f0b06146.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b625bf5af3944f3f4e95ed5d0b87f6b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9fe8ce981cd7dbe8704",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e0df78776f457786672f",
+    "url": "/static/js/2.652cd308.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.652cd308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9fe8ce981cd7dbe8704",
+    "url": "/static/js/main.40ce86e5.chunk.js"
+  },
+  {
+    "revision": "a3c2100dc2b01e1a257a",
+    "url": "/static/js/runtime-main.8a30fe64.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
